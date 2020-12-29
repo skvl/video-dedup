9dd5df1c9c18f1f4366ad11d4f490957c9fafa48
Sergey Kovalev <valor@list.ru>
2020-12-29 21:24:46 +0300

    Parse directory for duplicates

--- video-compare.sh
+++ video-compare.sh
@@ -3,14 +3,14 @@
 # set -Eeuxo pipefail
 
 function get_duration {
-	return `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 $1`
+	return `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "$1"`
 }
 
 function compare_by_duration {
-	get_duration $1
+	get_duration "$1"
 	local DURATION1=$?
 
-	get_duration $2
+	get_duration "$2"
 	local DURATION2=$?
 
 	if [ ${DURATION1} -eq ${DURATION2} ]
@@ -29,8 +29,8 @@ function compare_by_frames {
 	local W_FILE2=${WORK_DIR}/2.jpeg
 	local W_DIFF=${WORK_DIR}/diff.jpeg
 
-	ffmpeg -i ${FILE1} -ss 00:00:10 -frames:v 1 ${W_FILE1} 1>/dev/null 2>/dev/null
-	ffmpeg -i ${FILE2} -ss 00:00:10 -frames:v 1 ${W_FILE2} 1>/dev/null 2>/dev/null
+	ffmpeg -i "$1" -ss 00:00:10 -frames:v 1 ${W_FILE1} 1>/dev/null 2>/dev/null
+	ffmpeg -i "$2" -ss 00:00:10 -frames:v 1 ${W_FILE2} 1>/dev/null 2>/dev/null
 
 	local DELTA=`compare ${W_FILE1} ${W_FILE2} -format "%[distortion]" ${W_DIFF}`
 
@@ -46,36 +46,47 @@ function compare_by_frames {
 }
 
 function compare_files {
-	compare_by_duration ${FILE1} ${FILE2}
+	compare_by_duration "$1" "$2"
 	_result=$?
 	if [ ${_result} -eq 0 ]
 	then
-		echo "[DEBUG] ${FILE1} differ from ${FILE2} by duration"
+		echo "[DEBUG] \"$1\" differ from \"$2\" by duration"
 		return 1
 	else
-		compare_by_frames ${FILE1} ${FILE2}
+		compare_by_frames "$1" "$2"
 		_result=$?
 		if [ ${_result} -eq 0 ]
 		then
-			echo "[DEBUG] ${FILE1} differ from ${FILE2} by frames"
+			echo "[DEBUG] \"$1\" differ from \"$2\" by frames"
 			return 1
 		else
-			echo "[DEBUG] ${FILE1} looks equal to ${FILE2}"
+			echo "[DEBUG] \"$1\" looks equal to \"$2\""
 			return 0
 		fi
 	fi
 }
 
-FILE1=$1
-FILE2=$2
+array=()
+find $1 -print0 -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' >tmpfile
+while read p; do
+    array+=("$p")
+done <tmpfile
+rm -f tmpfile
 
-compare_files ${FILE1} ${FILE2}
-_result=$?
-if [ ${_result} -eq 1 ]
-then
-	echo "${FILE1} differ from ${FILE2}"
-	exit 1
-else
-	echo "${FILE1} looks equal to ${FILE2}"
-	exit 0
-fi
+echo "[DEBUG] Found ${#array[@]} video files"
+
+# iterate through array using a counter
+for ((i=0; i<${#array[@]}; i++)); do
+	for ((j=$i+1; j<${#array[@]}; j++)); do
+		echo "[DEBUG] Compare \"${array[$i]}\" with \"${array[$j]}\""
+
+		compare_files "${array[$i]}" "${array[$j]}"
+		_result=$?
+		if [ ${_result} -eq 1 ]
+		then
+			echo "\"${array[$i]}\" differ from \"${array[$j]}\""
+		else
+			echo "\"${array[$i]}\" looks equal to \"${array[$j]}\""
+		fi
+	done
+done
